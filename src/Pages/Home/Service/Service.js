5bac6d6df6434b803f271d40294da5e521836eb9
sazzad <hossensazzad1201@gmail.com>
2022-09-30 23:13:52 +0600

    home page designed

---
+++ src/Pages/Home/Service/Service.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Service.css'
+
+const Service = ({ service }) => {
+    const { name, price, description, img } = service
+    return (
+        <div className='service'>
+            <img src={img} alt="" />
+            <h2>{name}</h2>
+            <p>Price:{price}</p>
+            <p>Description:{description}</p>
+            <button className='btn btn-primary'>Book:{name}</button>
+
+        </div>
+    );
+};
+
+export default Service;
